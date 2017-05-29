ab6169ab311f6b9d8d14e64f93c6c5b357ed02e4
Matt McCormick <matt.mccormick@kitware.com>
2017-05-29 18:30:01 -0500

    linux: refactor ITK and module scripts to avoid duplicate code
    Suggested-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

---
+++ scripts/internal/manylinux-build-common.sh
@@ -0,0 +1,48 @@
+# Content common to manylinux-build-wheels.sh and
+# manylinux-build-module-wheels.sh
+
+set -e -x
+
+# Versions can be restricted by passing them in as arguments to the script
+# For example,
+# manylinux-build-wheels.sh cp27mu cp35
+if [[ $# -eq 0 ]]; then
+  PYBINARIES=(/opt/python/*/bin)
+else
+  PYBINARIES=()
+  for version in "$@"; do
+    PYBINARIES+=(/opt/python/*${version}*/bin)
+  done
+fi
+
+# i686 or x86_64 ?
+case $(uname -p) in
+    i686)
+        arch=x86
+        ;;
+    x86_64)
+        arch=x64
+        ;;
+    *)
+        die "Unknown architecture $(uname -p)"
+        ;;
+esac
+
+echo "Building wheels for $arch"
+
+# Since the python interpreter exports its symbol (see [1]), python
+# modules should not link against any python libraries.
+# To ensure it is not the case, we configure the project using an empty
+# file as python library.
+#
+# [1] "Note that libpythonX.Y.so.1 is not on the list of libraries that
+# a manylinux1 extension is allowed to link to. Explicitly linking to
+# libpythonX.Y.so.1 is unnecessary in almost all cases: the way ELF linking
+# works, extension modules that are loaded into the interpreter automatically
+# get access to all of the interpreter's symbols, regardless of whether or
+# not the extension itself is explicitly linked against libpython. [...]"
+#
+# Source: https://www.python.org/dev/peps/pep-0513/#libpythonx-y-so-1
+PYTHON_LIBRARY=$(cd $(dirname $0); pwd)/manylinux-libpython-not-needed-symbols-exported-by-interpreter
+touch ${PYTHON_LIBRARY}
+
--- scripts/internal/manylinux-build-wheels.sh
+++ scripts/internal/manylinux-build-wheels.sh
@@ -1,32 +1,7 @@
 #!/usr/bin/env bash
-set -e -x
 
-# Versions can be restricted by passing them in as arguments to the script
-# For example,
-# manylinux-build-wheels.sh cp27mu cp35
-if [[ $# -eq 0 ]]; then
-  PYBINARIES=(/opt/python/*/bin)
-else
-  PYBINARIES=()
-  for version in "$@"; do
-    PYBINARIES+=(/opt/python/*${version}*/bin)
-  done
-fi
-
-# i686 or x86_64 ?
-case $(uname -p) in
-    i686)
-        arch=x86
-        ;;
-    x86_64)
-        arch=x64
-        ;;
-    *)
-        die "Unknown architecture $(uname -p)"
-        ;;
-esac
-
-echo "Building wheels for $arch"
+script_dir="`cd $(dirname $0); pwd`"
+source "${script_dir}/manylinux-build-common.sh"
 
 # Build standalone project and populate archive cache
 mkdir -p /work/standalone-${arch}-build
@@ -34,22 +9,6 @@ pushd /work/standalone-${arch}-build > /dev/null 2>&1
   cmake -DITKPythonPackage_BUILD_PYTHON:PATH=0 -G Ninja ../
   ninja
 popd > /dev/null 2>&1
-
-# Since the python interpreter exports its symbol (see [1]), python
-# modules should not link against any python libraries.
-# To ensure it is not the case, we configure the project using an empty
-# file as python library.
-#
-# [1] "Note that libpythonX.Y.so.1 is not on the list of libraries that
-# a manylinux1 extension is allowed to link to. Explicitly linking to
-# libpythonX.Y.so.1 is unnecessary in almost all cases: the way ELF linking
-# works, extension modules that are loaded into the interpreter automatically
-# get access to all of the interpreter's symbols, regardless of whether or
-# not the extension itself is explicitly linked against libpython. [...]"
-#
-# Source: https://www.python.org/dev/peps/pep-0513/#libpythonx-y-so-1
-PYTHON_LIBRARY=/work/scripts/internal/manylinux-libpython-not-needed-symbols-exported-by-interpreter
-touch ${PYTHON_LIBRARY}
 
 # Compile wheels re-using standalone project and archive cache
 for PYBIN in "${PYBINARIES[@]}"; do
