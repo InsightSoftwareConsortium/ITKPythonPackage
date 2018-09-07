0eaddb4ed2f0e44e65ca80b82361c8b33ab8efa6
Matt McCormick <matt.mccormick@kitware.com>
2018-09-07 19:18:12 -0400

    external module builds: Build against ITK 5.0 Beta 1

--- scripts/dockcross-manylinux-download-cache-and-build-module-wheels.sh
+++ scripts/dockcross-manylinux-download-cache-and-build-module-wheels.sh
@@ -7,7 +7,7 @@ wget -L https://data.kitware.com/api/v1/file/592dd8068d777f16d01e1a92/download -
 gunzip -d zstd-1.2.0-linux.tar.gz
 tar xf zstd-1.2.0-linux.tar
 
-curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.0a02}/ITKPythonBuilds-linux.tar.zst -O
+curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.0b01}/ITKPythonBuilds-linux.tar.zst -O
 ./zstd-1.2.0-linux/bin/unzstd ITKPythonBuilds-linux.tar.zst -o ITKPythonBuilds-linux.tar
 tar xf ITKPythonBuilds-linux.tar
 
