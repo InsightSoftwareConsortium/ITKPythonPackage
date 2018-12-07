545f45c39dd77d96ebf3f18b3d06fcdbb5f63aec
Matt McCormick <matt.mccormick@kitware.com>
2018-12-07 15:28:14 -0500

    scripts: Use curl instead of wget for downloads for newer SSL support

--- scripts/dockcross-manylinux-download-cache-and-build-module-wheels.sh
+++ scripts/dockcross-manylinux-download-cache-and-build-module-wheels.sh
@@ -3,7 +3,7 @@
 # This module should be pulled and run from an ITKModule root directory to generate the Linux python wheels of this module,
 # it is used by the circle.yml file contained in ITKModuleTemplate: https://github.com/InsightSoftwareConsortium/ITKModuleTemplate
 
-wget -L https://data.kitware.com/api/v1/file/592dd8068d777f16d01e1a92/download -O zstd-1.2.0-linux.tar.gz
+curl https://data.kitware.com/api/v1/file/592dd8068d777f16d01e1a92/download -o zstd-1.2.0-linux.tar.gz
 gunzip -d zstd-1.2.0-linux.tar.gz
 tar xf zstd-1.2.0-linux.tar
 
@@ -12,7 +12,7 @@ curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/do
 tar xf ITKPythonBuilds-linux.tar
 
 mkdir tools
-wget -L https://data.kitware.com/api/v1/file/5c0aa4b18d777f2179dd0a71/download -O doxygen-1.8.11.linux.bin.tar.gz
+curl https://data.kitware.com/api/v1/file/5c0aa4b18d777f2179dd0a71/download -o doxygen-1.8.11.linux.bin.tar.gz
 tar -xvzf doxygen-1.8.11.linux.bin.tar.gz -C tools
 
 ./ITKPythonPackage/scripts/dockcross-manylinux-build-module-wheels.sh
