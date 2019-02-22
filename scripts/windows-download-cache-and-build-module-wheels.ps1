679ff1a2b67f6918d1a022a0706eab7e046a9e30
Francois Budin <francois.budin@kitware.com>
2019-02-22 10:12:39 -0500

    ENH: Windows download script patches IPP archive to change Python versions
    * Disable Python 2.7
    * Enable Python 3.7

--- scripts/windows-download-cache-and-build-module-wheels.ps1
+++ scripts/windows-download-cache-and-build-module-wheels.ps1
@@ -7,6 +7,7 @@ iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.co
 if (-not (Test-Path env:ITK_PACKAGE_VERSION)) { $env:ITK_PACKAGE_VERSION = 'v5.0a01' }
 Invoke-WebRequest -Uri "https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/$env:ITK_PACKAGE_VERSION/ITKPythonBuilds-windows.zip" -OutFile "ITKPythonBuilds-windows.zip"
 sz x ITKPythonBuilds-windows.zip -oC:\P -aoa -r
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/InsightSoftwareConsortium/ITKPythonPackage/b2974be9f53a6e99edc4c317b3992362a36f24b2/scripts/internal/windows_build_common.py" -OutFile "C:\P\IPP\scripts\internal\windows_build_common.py"
 Invoke-WebRequest -Uri "https://data.kitware.com/api/v1/file/5c0ad59d8d777f2179dd3e9c/download" -OutFile "doxygen-1.8.11.windows.bin.zip"
 sz x doxygen-1.8.11.windows.bin.zip -oC:\P\doxygen -aoa -r
 Invoke-WebRequest -Uri "https://data.kitware.com/api/v1/file/5bbf87ba8d777f06b91f27d6/download/grep-win.zip" -OutFile "grep-win.zip"
