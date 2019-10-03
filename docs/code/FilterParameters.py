19a5b80a01a31261cf6439dba4eadd41171be6b6
Matt McCormick <matt.mccormick@kitware.com>
2019-10-03 11:57:41 -0400

    doc: Describe how to set filter parameters

---
+++ docs/code/FilterParameters.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import itk
+import sys
+
+input_filename = sys.argv[1]
+
+image = itk.imread(input_filename, itk.F)
+
+# Pythonic snake case keyword arguments:
+#
+#   number_of_iterations
+#
+smoothed = itk.anti_alias_binary_image_filter(image,
+        number_of_iterations=3)
+
+# CamelCase keyword arguments:
+#
+#   NumberOfIterations
+#
+smoother = itk.AntiAliasBinaryImageFilter.New(image,
+        NumberOfIterations=3)
+smoother.Update()
+smoothed = smoother.GetOutput()
+
+# CamelCase Set method:
+#
+#   SetNumberOfIterations
+#
+smoother = itk.AntiAliasBinaryImageFilter.New(image)
+smoother.SetNumberOfIterations(3)
+smoother.Update()
+smoothed = smoother.GetOutput()
