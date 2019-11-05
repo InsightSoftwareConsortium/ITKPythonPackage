799c8ca450f06b5b68586468b4c072d12e55f351
Matt McCormick <matt.mccormick@kitware.com>
2019-11-05 11:43:32 -0500

    scripts: Add macOSX SDK symlink
    Still required for the Python builds

--- scripts/macpython-download-cache-and-build-module-wheels.sh
+++ scripts/macpython-download-cache-and-build-module-wheels.sh
@@ -6,6 +6,12 @@
 brew update
 brew install zstd aria2 gnu-tar doxygen
 brew upgrade cmake
+# The Azure systems current do not have the 10.13 SDK, required by the
+# ITKPythonBuilds
+SDK_PATH='/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk'
+if test ! -e $SDK_PATH; then
+  sudo ln -s /Applications/Xcode_9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk
+fi
 aria2c -c --file-allocation=none -o ITKPythonBuilds-macosx.tar.zst -s 10 -x 10 https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.0.1}/ITKPythonBuilds-macosx.tar.zst
 unzstd ITKPythonBuilds-macosx.tar.zst -o ITKPythonBuilds-macosx.tar
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
