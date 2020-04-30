0348b6cc5a2574d756cfdac39079131f3952fd3f
Matt McCormick <matt.mccormick@kitware.com>
2020-04-30 17:34:59 -0400

    scripts: Specify pyenvs to windows_build_module_wheels

--- scripts/windows_build_module_wheels.py
+++ scripts/windows_build_module_wheels.py
@@ -1,6 +1,7 @@
 from subprocess import check_call
 import os
 import sys
+import argparse
 
 SCRIPT_DIR = os.path.dirname(__file__)
 ROOT_DIR = os.path.abspath(os.getcwd())
@@ -14,7 +15,7 @@ sys.path.insert(0, os.path.join(SCRIPT_DIR, "internal"))
 from wheel_builder_utils import push_dir, push_env
 from windows_build_common import DEFAULT_PY_ENVS, venv_paths
 
-def build_wheels(py_envs=DEFAULT_PY_ENVS):
+def build_wheels(py_envs=DEFAULT_PY_ENVS, cleanup=True, cmake_options=[]):
     for py_env in py_envs:
         python_executable, \
                 python_include_dir, \
@@ -53,9 +54,17 @@ def build_wheels(py_envs=DEFAULT_PY_ENVS):
                 "-DPython3_INCLUDE_DIR:PATH=%s" % python_include_dir,
                 "-DPython3_INCLUDE_DIRS:PATH=%s" % python_include_dir,
                 "-DPython3_LIBRARY:FILEPATH=%s" % python_library
-            ])
+            ] + cmake_options)
             # Cleanup
-            check_call([python_executable, "setup.py", "clean"])
+            if cleanup:
+                check_call([python_executable, "setup.py", "clean"])
 
 if __name__ == '__main__':
-    build_wheels()
+    parser = argparse.ArgumentParser(description='Driver script to build ITK Python module wheels.')
+    parser.add_argument('--py-envs', nargs='+', default=DEFAULT_PY_ENVS,
+            help='Target Python environment versions, e.g. "37-x64".')
+    parser.add_argument('--no-cleanup', dest='cleanup', action='store_false', help='Do not clean up temporary build files.')
+    parser.add_argument('cmake_options', nargs='*', help='Extra options to pass to CMake, e.g. -DBUILD_SHARED_LIBS:BOOL=OFF')
+    args = parser.parse_args()
+
+    build_wheels(cleanup=args.cleanup, py_envs=args.py_envs, cmake_options=args.cmake_options)
