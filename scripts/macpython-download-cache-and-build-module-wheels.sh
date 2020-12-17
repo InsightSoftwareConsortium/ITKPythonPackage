e5a8f2aa5f5646cdb1f941f81665f240ded0f4b5
svc-dashboard <svc-dashboard@computron.kitware.com>
2020-12-17 14:56:39 -0500

    macos: Update build directory
    We now use a GitHub Actions runner compliant code path.
    
    https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md
    	/Applications/Xcode_11.7.app

--- scripts/macpython-download-cache-and-build-module-wheels.sh
+++ scripts/macpython-download-cache-and-build-module-wheels.sh
@@ -6,17 +6,11 @@
 brew update
 brew install zstd aria2 gnu-tar doxygen
 brew upgrade cmake
-# The Azure systems current do not have the 10.13 SDK, required by the
-# ITKPythonBuilds
-SDK_PATH='/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk'
-if test ! -e $SDK_PATH; then
-  sudo ln -s /Applications/Xcode_9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk
-fi
 aria2c -c --file-allocation=none -o ITKPythonBuilds-macosx.tar.zst -s 10 -x 10 https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.1.0}/ITKPythonBuilds-macosx.tar.zst
 unzstd ITKPythonBuilds-macosx.tar.zst -o ITKPythonBuilds-macosx.tar
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 tar xf ITKPythonBuilds-macosx.tar --checkpoint=10000 --checkpoint-action=dot
 rm ITKPythonBuilds-macosx.tar
-sudo mkdir -p /Users/Kitware/Dashboards/ITK && sudo chown $UID:$GID /Users/Kitware/Dashboards/ITK && mv ITKPythonPackage /Users/Kitware/Dashboards/ITK/
-/Users/Kitware/Dashboards/ITK/ITKPythonPackage/scripts/macpython-install-python.sh
-/Users/Kitware/Dashboards/ITK/ITKPythonPackage/scripts/macpython-build-module-wheels.sh "$@"
+sudo mkdir -p /Users/svc-dashboard/D/P && sudo chown $UID:$GID /Users/svc-dashboard/D/P && mv ITKPythonPackage /Users/svc-dashboard/D/P/
+/Users/svc-dashboard/D/P/ITKPythonPackage/scripts/macpython-install-python.sh
+/Users/svc-dashboard/D/P/ITKPythonPackage/scripts/macpython-build-module-wheels.sh "$@"
