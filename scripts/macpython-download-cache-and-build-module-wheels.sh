c5ffdad02e623d21cc43b0d2005b3e70e32b286b
Matt McCormick <matt.mccormick@kitware.com>
2020-12-18 14:35:04 -0500

    macos: Remove old Python versions before installing new
    Avoid trying to build against Python 2.7

--- scripts/macpython-download-cache-and-build-module-wheels.sh
+++ scripts/macpython-download-cache-and-build-module-wheels.sh
@@ -6,11 +6,12 @@
 brew update
 brew install zstd aria2 gnu-tar doxygen
 brew upgrade cmake
-aria2c -c --file-allocation=none -o ITKPythonBuilds-macosx.tar.zst -s 10 -x 10 https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.1.0}/ITKPythonBuilds-macosx.tar.zst
+aria2c -c --file-allocation=none -o ITKPythonBuilds-macosx.tar.zst -s 10 -x 10 https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/${ITK_PACKAGE_VERSION:=v5.1.2}/ITKPythonBuilds-macosx.tar.zst
 unzstd ITKPythonBuilds-macosx.tar.zst -o ITKPythonBuilds-macosx.tar
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 tar xf ITKPythonBuilds-macosx.tar --checkpoint=10000 --checkpoint-action=dot
 rm ITKPythonBuilds-macosx.tar
 sudo mkdir -p /Users/svc-dashboard/D/P && sudo chown $UID:$GID /Users/svc-dashboard/D/P && mv ITKPythonPackage /Users/svc-dashboard/D/P/
+sudo rm -rf /Library/Frameworks/Python.framework/Versions/*
 /Users/svc-dashboard/D/P/ITKPythonPackage/scripts/macpython-install-python.sh
 /Users/svc-dashboard/D/P/ITKPythonPackage/scripts/macpython-build-module-wheels.sh "$@"
