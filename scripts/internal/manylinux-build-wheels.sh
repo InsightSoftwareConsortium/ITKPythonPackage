fdec0dfd1191ecbe1d5c255ecd11d905568ddb04
Matt McCormick <matt.mccormick@kitware.com>
2021-04-24 21:35:50 -0400

    BUG: doxygen installation for dockcross manylinux

--- scripts/internal/manylinux-build-wheels.sh
+++ scripts/internal/manylinux-build-wheels.sh
@@ -9,22 +9,7 @@ Python3_LIBRARY=""
 
 script_dir=$(cd $(dirname $0) || exit 1; pwd)
 source "${script_dir}/manylinux-build-common.sh"
-# Install prerequirements
-yum install -y doxygen
-if ! type ninja > /dev/null 2>&1; then
-  git clone git://github.com/ninja-build/ninja.git
-  pushd ninja
-  git checkout release
-  cmake -Bbuild-cmake -H.
-  cmake --build build-cmake
-  cp build-cmake/ninja /usr/local/bin/
-  popd
-fi
-#mkdir -p /work/tools
-#pushd /work/tools > /dev/null 2>&1
-#curl https://data.kitware.com/api/v1/file/5c0aa4b18d777f2179dd0a71/download -o doxygen-1.8.11.linux.bin.tar.gz
-#tar -xvzf doxygen-1.8.11.linux.bin.tar.gz
-#popd > /dev/null 2>&1
+
 # -----------------------------------------------------------------------
 
 # Build standalone project and populate archive cache
@@ -82,7 +67,7 @@ for PYBIN in "${PYBINARIES[@]}"; do
             -DCMAKE_BUILD_TYPE:STRING="${build_type}" \
             -DPython3_EXECUTABLE:FILEPATH=${Python3_EXECUTABLE} \
             -DPython3_INCLUDE_DIR:PATH=${Python3_INCLUDE_DIR} \
-            -DITK_WRAP_DOC:BOOL=ON \
+            -DITK_WRAP_DOC:BOOL=ON
       # Cleanup
       ${PYBIN}/python setup.py clean
 
