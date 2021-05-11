600371c3125e9a1b81b9a4629d8109aad6c09096
Matt McCormick <matt.mccormick@kitware.com>
2021-05-11 19:16:44 -0400

    BUG: Provide permissioned tools directory for linux module build
    To address:
    
    ++ pushd /work/tools
    ++ curl https://data.kitware.com/api/v1/file/5c0aa4b18d777f2179dd0a71/download -o doxygen-1.8.11.linux.bin.tar.gz
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    
      0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0Warning: Failed to create the file doxygen-1.8.11.linux.bin.tar.gz: Permission
    Warning: denied
    
      0 15.1M    0  3744    0     0  16334      0  0:16:10 --:--:--  0:16:10 21033
    curl: (23) Failed writing body (0 != 3744)

--- scripts/dockcross-manylinux-build-module-wheels.sh
+++ scripts/dockcross-manylinux-build-module-wheels.sh
@@ -14,6 +14,8 @@ chmod u+x /tmp/dockcross-manylinux-x64
 
 script_dir=$(cd $(dirname $0) || exit 1; pwd)
 
+mkdir -p $(pwd)/tools
+chmod 777 $(pwd)/tools
 # Build wheels
 mkdir -p dist
 DOCKER_ARGS="-v $(pwd)/dist:/work/dist/ -v $script_dir/..:/ITKPythonPackage -v $(pwd)/tools:/tools"
