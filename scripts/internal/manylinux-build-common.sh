64b79cb1ca5f926fa07e324621abaf3c22f95449
Matt McCormick <matt.mccormick@kitware.com>
2021-09-07 23:23:40 -0400

    ENH: Do not build 3.6 wheels on Linux

--- scripts/internal/manylinux-build-common.sh
+++ scripts/internal/manylinux-build-common.sh
@@ -16,7 +16,7 @@ if [[ $# -eq 0 ]]; then
   PYBIN=(/opt/python/*/bin)
   PYBINARIES=()
   for version in "${PYBIN[@]}"; do
-    if [[ ${version} == *"cp36"* || ${version} == *"cp37"* || ${version} == *"cp38"* || ${version} == *"cp39"* ]]; then
+    if [[ ${version} == *"cp37"* || ${version} == *"cp38"* || ${version} == *"cp39"* ]]; then
       PYBINARIES+=(${version})
     fi
   done
