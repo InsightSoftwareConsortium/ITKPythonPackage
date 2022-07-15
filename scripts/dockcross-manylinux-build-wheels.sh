fd8e0868a23801447c5b3141298bd9768bf5099f
Matt McCormick <matt.mccormick@kitware.com>
2022-07-15 09:09:07 -0400

    BUG: Use manylinux_2_28 with tagged wheel script

--- scripts/dockcross-manylinux-build-wheels.sh
+++ scripts/dockcross-manylinux-build-wheels.sh
@@ -8,7 +8,7 @@
 #   scripts/dockcross-manylinux-build-wheels.sh cp39
 
 MANYLINUX_VERSION=_2_28
-IMAGE_TAG=20220705-b1eb184
+IMAGE_TAG=20220715-9ce3707
 
 # Generate dockcross scripts
 docker run --rm dockcross/manylinux${MANYLINUX_VERSION}-x64:${IMAGE_TAG} > /tmp/dockcross-manylinux-x64
