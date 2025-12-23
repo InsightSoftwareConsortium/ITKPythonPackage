2d669500e3ae2e1d2233fa67c540e959c78b627f
Hans J. Johnson <hans-johnson@uiowa.edu>
2025-12-23 09:00:40 -0600

    REF: Reformat `from __future__ import annotations` for improved readability

--- scripts/pyproject_configure.py
+++ scripts/pyproject_configure.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-from __future__ import annotations # Needed for python 3.9 to support python 3.10 style typehints
+from __future__ import (
+    annotations,
+)  # Needed for python 3.9 to support python 3.10 style typehints
 
 import argparse
 import os
