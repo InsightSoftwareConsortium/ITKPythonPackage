c731493a3c80dd64fc31c1372279f95a23be9093
Hans Johnson <hans-johnson@uiowa.edu>
2025-12-24 13:46:23 -0600

    ENH: Refactor wheel build scripts to simplify environment
    handling and improve manylinux configuration

--- scripts/dockcross-manylinux-build-wheels.sh
+++ scripts/dockcross-manylinux-build-wheels.sh
@@ -16,27 +16,26 @@
 #
 script_dir=$(cd $(dirname $0) || exit 1; pwd)
 _ipp_dir=$(dirname ${script_dir})
-package_env_file=${_ipp_dir}/build/package.env
-if [ ! -f "${package_env_file}" ]; then
-  # generate_build_environment.py respects environmental variables set by
-  # ITKRemoteModuleBuildTestPackageAction/.github/workflows/build-test-package-python.yml
-  ${_ipp_dir}/scripts/generate_build_environment.py -o ${package_env_file}
-else
-  # Re-use cached package_env_file
-  ${_ipp_dir}/scripts/generate_build_environment.py -i ${package_env_file} -o ${package_env_file}
-fi
-source "${package_env_file}"
+
+
+
+#TODO: This needs updating to pass along values to
+ITK_GIT_TAG=${ITK_GIT_TAG:="main"}
+MANYLINUX_VERSION=${MANYLINUX_VERSION:=_2_28}
+IMAGE_TAG=${IMAGE_TAG:=20250913-6ea98ba}
+TARGET_ARCH=${TARGET_ARCH:=x64}
+ITKPYTHONPACKAGE_ORG=${ITKPYTHONPACKAGE_ORG:=InsightSoftwareConsortium}
+ITKPYTHONPACKAGE_TAG=${ITKPYTHONPACKAGE_TAG:=main}
 
 # Required environment variables
 required_vars=(
   ITK_GIT_TAG
+  MANYLINUX_VERSION
+  IMAGE_TAG
+  TARGET_ARCH
   ITKPYTHONPACKAGE_ORG
   ITKPYTHONPACKAGE_TAG
-  IMAGE_TAG
-  MANYLINUX_VERSION
-  TARGET_ARCH
 )
-
 # Sanity Validation loop
 for v in "${required_vars[@]}"; do
   if [ -z "${!v:-}" ]; then
@@ -56,20 +55,19 @@ CONTAINER_WORK_DIR=/work
 CONTAINER_PACKAGE_DIST=${CONTAINER_WORK_DIR}/dist
 CONTAINER_PACKAGE_BUILD_DIR=${CONTAINER_WORK_DIR}/ITK-source
 CONTAINER_ITK_SOURCE_DIR=${CONTAINER_PACKAGE_BUILD_DIR}/ITK
-CONTAINER_ENV_FILE=${CONTAINER_PACKAGE_DIST}/container_package.env
 
 HOST_PACKAGE_DIST=${_ipp_dir}/dist
-HOST_TO_CONTAINER_ENV_FILE=${HOST_PACKAGE_DIST}/container_package.env
 mkdir -p ${HOST_PACKAGE_DIST}
-#HOST_PACKAGE_BUILD_DIR=${_ipp_dir}/ITK-source_manylinux${MANYLINUX_VERSION}-x64_${IMAGE_TAG}
-#mkdir -p ${HOST_PACKAGE_BUILD_DIR}
 
-# Need to fixup path for container to find ITK, TODO: build_wheels.py should take in optional .env file
-sed "s#ITK_SOURCE_DIR=.*#ITK_SOURCE_DIR=${CONTAINER_ITK_SOURCE_DIR}#g"  ${package_env_file} \
-  | sed "s#DOXYGEN_EXECUTABLE=.*##g" - \
-  | sed "s#NINJA_EXECUTABLE=.*##g"   - \
-  | sed "s#CMAKE_EXECUTABLE=.*##g"   - > ${HOST_TO_CONTAINER_ENV_FILE}
-mv ${package_env_file} ${package_env_file}_hidden
+
+
+DOCKER_ARGS="  -v ${_ipp_dir}/dist:${CONTAINER_WORK_DIR}/dist/ "
+if [ "${ITK_SOURCE_DIR}" -ne "" ]; then
+  DOCKER_ARGS+=" -v${ITK_SOURCE_DIR}:${CONTAINER_ITK_SOURCE_DIR} "
+fi
+DOCKER_ARGS+=" -e PYTHONUNBUFFERED=1 " # Turn off buffering of outputs in python
+
+BUILD_WHEELS_EXTRA_FLAGS=${BUILD_WHEELS_EXTRA_FLAGS:=" --build-itk-tarball-cache --no-cleanup "}
 
 #If command line argument given, then use them
 if [ $# -ge 1 ]; then
@@ -78,25 +76,18 @@ else
   PY_ENVS="3.9 3.10 3.11"
 fi
 
-DOCKER_ARGS="  -v ${_ipp_dir}/dist:${CONTAINER_WORK_DIR}/dist/ "
-DOCKER_ARGS+=" -v${ITK_SOURCE_DIR}:${CONTAINER_ITK_SOURCE_DIR} "
-DOCKER_ARGS+=" --env-file ${HOST_TO_CONTAINER_ENV_FILE} "  # Configure container to start with correct environment variables
-DOCKER_ARGS+=" -e PACKAGE_ENV_FILE=${CONTAINER_ENV_FILE} " # Choose the right env cache inside container (needed for subsequent scripts)
-DOCKER_ARGS+=" -e PYTHONUNBUFFERED=1 " # Turn off buffering of outputs in python
-
-BUILD_WHEELS_EXTRA_FLAGS=${BUILD_WHEELS_EXTRA_FLAGS:=" --build-itk-tarball-cache --no-cleanup "}
-#BUILD_WHEELS_EXTRA_FLAGS=\"${BUILD_WHEELS_EXTRA_FLAGS}\" \
-PIXI_ENV=${PIXI_ENV:=manylinux228}
-
 # When building ITK wheels, --module-source-dir, --module-dependancies-root-dir, and --itk-module-deps to be empty
 cmd=$(echo bash -x ${_local_dockercross_script} \
   -a \"$DOCKER_ARGS\" \
    /usr/bin/env \
-     PIXI_ENV=\"${PIXI_ENV}\" \
      PY_ENVS=\"${PY_ENVS}\" \
+     ITK_GIT_TAG=\"${ITK_GIT_TAG}\" \
+     MANYLINUX_VERSION=\"${MANYLINUX_VERSION}\" \
+     IMAGE_TAG=\"${IMAGE_TAG}\" \
+     TARGET_ARCH=\"${TARGET_ARCH}\" \
+     ITKPYTHONPACKAGE_ORG=\"${ITKPYTHONPACKAGE_ORG}\" \
+     ITKPYTHONPACKAGE_TAG=\"${ITKPYTHONPACKAGE_ORG}\" \
      /bin/bash -x ${CONTAINER_WORK_DIR}/scripts/docker_build_environment_driver.sh
 )
 echo "RUNNING: $cmd"
 eval $cmd
-
-mv ${package_env_file}_hidden ${package_env_file}
--- scripts/docker_build_environment_driver.sh
+++ scripts/docker_build_environment_driver.sh
@@ -3,30 +3,49 @@
 CONTAINER_WORK_DIR=/work
 cd ${CONTAINER_WORK_DIR}
 
+
+# Required environment variables
+required_vars=(
+  ITK_GIT_TAG
+  MANYLINUX_VERSION
+  IMAGE_TAG
+  TARGET_ARCH
+  ITKPYTHONPACKAGE_ORG
+  ITKPYTHONPACKAGE_TAG
+  PY_ENVS
+)
+# Sanity Validation loop
+for v in "${required_vars[@]}"; do
+  if [ -z "${!v:-}" ]; then
+    echo "ERROR: Required environment variable '$v' is not set or empty."
+    exit 1
+  fi
+done
+
 CONTAINER_PACKAGE_DIST=${CONTAINER_WORK_DIR}/dist
-CONTAINER_ENV_FILE=${CONTAINER_PACKAGE_DIST}/container_package.env
 
 # Set BUILD_WHEELS_EXTRA_FLAGS="" to disable building the tarball or force cleanup
-BUILD_WHEELS_EXTRA_FLAGS=${BUILD_WHEELS_EXTRA_FLAGS:=" --build-itk-tarball-cache --no-cleanup "}
-PIXI_ENV=${PIXI_ENV:=manylinux228}
-PY_ENVS=${PY_ENVS:=3.11}
+BUILD_WHEELS_EXTRA_FLAGS=${BUILD_WHEELS_EXTRA_FLAGS:=" --build-itk-tarball-cache "}
 
-# Use pixi to ensure all required tools are installed and
-# visible in the PATH
-export PIXI_HOME=${CONTAINER_WORK_DIR}/.pixi
-export PATH=${PIXI_HOME}/bin:${PATH}
-pixi install -e ${PIXI_ENV}
+echo "BUILD FOR ${PY_ENVS}"
 
-CONTAINER_ENV_FILE=${CONTAINER_PACKAGE_DIST}/container_package.env
-echo DOXYGEN_EXECUTABLE=$(pixi run -e manylinux228 which doxygen) >> ${CONTAINER_ENV_FILE}
-echo NINJA_EXECUTABLE=$(pixi run -e manylinux228 which ninja) >> ${CONTAINER_ENV_FILE}
-echo CMAKE_EXECUTABLE=$(pixi run -e manylinux228 which cmake) >> ${CONTAINER_ENV_FILE}
-# Set the path from within the container for the TBB_DIR
-echo "TBB_DIR=${CONTAINER_WORK_DIR}/build/oneTBB-prefix/lib/cmake/TBB" >> ${CONTAINER_ENV_FILE}
-echo "LD_LIBRARY_DIR=${CONTAINER_WORK_DIR}/build/oneTBB-prefix/lib" >> ${CONTAINER_ENV_FILE}
+for pyver in ${PY_ENVS}; do
+  py_vername=$(echo ${pyver} |sed 's/\.//g')
+  manylinux_vername=$(echo ${MANYLINUX_VERSION} |sed 's/_//g')
+  PIXI_ENV="manylinux${manylinux_vername}-py${py_vername}"
 
-pixi run -e ${PIXI_ENV} python3.13 \
-  ${CONTAINER_WORK_DIR}/scripts/build_wheels.py \
-  --py-envs ${PY_ENVS} \
-  ${BUILD_WHEELS_EXTRA_FLAGS} \
-  --package-env-file ${CONTAINER_ENV_FILE}
+  # Use pixi to ensure all required tools are installed and
+  # visible in the PATH
+  export PIXI_HOME=${CONTAINER_WORK_DIR}/.pixi
+  export PATH=${PIXI_HOME}/bin:${PATH}
+  python3.12 ${CONTAINER_WORK_DIR}/scripts/install_pixi.py --platform-env ${PIXI_ENV}
+
+  pixi run -e ${PIXI_ENV} python3 \
+    ${CONTAINER_WORK_DIR}/scripts/build_wheels.py \
+    --platform-env ${PIXI_ENV} \
+    ${BUILD_WHEELS_EXTRA_FLAGS} \
+   --build-dir-root ${CONTAINER_WORK_DIR} \
+   --itk-git-tag ${ITK_GIT_TAG} \
+   --manylinux-version ${MANYLINUX_VERSION} \
+
+done
